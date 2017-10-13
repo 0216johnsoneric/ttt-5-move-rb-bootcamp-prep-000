5434516c21c98f77499472fd468b464bb76621c2
Eric Johnson <0216johnsoneric@gmail.com>
2017-10-13 20:23:55 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,8 +12,8 @@ def input_to_index(move)
    index
 end
 
-def move(board, index, token)
-  puts token = X
+def move(board, index, token = X)
+  
   # puts board[index] = token
   # puts play(board)
 end
